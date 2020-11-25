dc1bcc1d3304ae54ba074e1fd29c05b194300fbf
Rajabov Bobokhon <rboboxon6055@gmail.com>
2020-11-25 22:55:01 +0900

    fix: integrate local storage

--- app.js
+++ app.js
@@ -1,144 +1,122 @@
- 
 // Wrap every letter in a span
-var textWrapper = document.querySelector('.ml9 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+// var textWrapper = document.querySelector('.ml9 .letters');
+// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: true})
+anime
+  .timeline({ loop: true })
   .add({
-    targets: '.ml9 .letter',
+    targets: ".ml9 .letter",
     scale: [0, 1],
     duration: 1500,
     elasticity: 600,
-    delay: (el, i) => 45 * (i+1)
-  }).add({
-    targets: '.ml9',
+    delay: (el, i) => 45 * (i + 1),
+  })
+  .add({
+    targets: ".ml9",
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
-    delay: 1000
+    delay: 1000,
   });
 
+//get stated with getting buutonn and input
 
-  //get stated with getting buutonn and input
+const inputValue = document.getElementById("inputValue");
+const addButton = document.getElementById("addButton");
+// const inputResult = document.getElementById('inputResult');
+const form = document.getElementById("formAdd");
+const items = document.getElementById("items");
 
-  const inputValue = document.getElementById("inputValue");
-  const addButton = document.getElementById("addButton");
-  // const inputResult = document.getElementById('inputResult');
-  const form = document.getElementById("formAdd");
-  const items = document.getElementById('items');
+document.addEventListener("DOMContentLoaded", () => {
+  const savedTasks = handleLocal.getLocalItems();
 
+  savedTasks.forEach((task) => {
+    addItemToHTML(task);
+  });
+});
 
+inputValue.addEventListener("keydown", (e) => {
+  e.key === "Enter" && addtask();
+});
 
-  addButton.addEventListener("click", addtask);
+addButton.addEventListener("click", addtask);
 
-  function addtask(){
+function addtask() {
+  if (inputValue.value === "" || inputValue.value.trim() === "") {
+    alert("Please write your task, It is empty!!!");
+  } else {
+    //add task to HTML document
+    addItemToHTML(inputValue.value);
 
-    if(inputValue.value === ""){
-      alert("Please write your task, It is empty!!!")
-    }else{
-      
-       //local
-    
-      
-      // const toDoDiv = document.createElement('div');
-      // toDoDiv.classList.add("todo")
-       //////List items UI
-      let newItem = inputValue.value;
-      let li =  document.createElement('li');
-      
-      li.className = "list-group-item  mt-3 mr-5 w-50 mr-5  mx-auto align-items-center justify-content-center text-left";
+    // save the task on local storage
+    handleLocal.setLocalItem(inputValue.value);
 
-      let deleteButton =  document.createElement('button');
-      
-      deleteButton.appendChild(document.createTextNode('Delete'));
-      deleteButton.className = "btn-danger btn btn-sm float-right text-right delete";
-  
-      
-    li.appendChild(document.createTextNode(newItem));
-    li.appendChild(deleteButton);
-
-    items.appendChild(li);
-    
-
-    
-//local saved
-    
-
-    //remove item
-    deleteButton.addEventListener('click', removeListItem);
-
-    function removeListItem(e){
-      e.preventDefault();
-
-      if(e.target.classList.contains('delete')){
-        let li = e.target.parentNode;
-        items.removeChild(li);
-      }
-    
-      
-      
-    }
-    //local
-   
+    //empty the form input
     inputValue.value = "";
-    
-    }
-   
   }
-   //retrive local data to dom
-  //  function getList(){ 
-  //   let tasks;
-        
-  //   if(localStorage.getItem('tasks') === null){
-  //     tasks = []; 
-  //   }else{
-  //     tasks = JSON.parse(localStorage.getItem("tasks"))
-  //   }
- 
-  //   pushing to array
-  //   tasks.push(task);
- 
-  //   localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
-  //   retive
-  //   tasks.forEach(function(task){
-  //     let newItem = inputValue.value;
-  //     let li =  document.createElement('li');
-      
-  //     li.className = "list-group-item  mt-3 mr-5 w-50 mr-5  mx-auto align-items-center justify-content-center text-left";
+const addItemToHTML = (task) => {
+  //////List items UI
 
-  //     let deleteButton =  document.createElement('button');
-      
-  //     deleteButton.appendChild(document.createTextNode('Delete'));
-  //     deleteButton.className = "btn-danger btn btn-sm float-right text-right delete";
-  
-      
-  //   li.appendChild(document.createTextNode(task));
-  //   li.appendChild(deleteButton);
+  let li = document.createElement("li");
 
-  //   items.appendChild(li);
-  //   })
-  //  }
- 
+  li.className =
+    "list-group-item  mt-3 mr-5 w-50 mr-5  mx-auto align-items-center justify-content-center text-left";
 
-  // // add
-  //  //addd loacal stoarge
-  //  function addlocal(task){
-  //   let tasks;
-        
-  //   if(localStorage.getItem('tasks') === null){
-  //     tasks = []; 
-  //   }else{
-  //     tasks = JSON.parse(localStorage.getItem("tasks"))
-  //   }
- 
-  //   //pushing to array
-  //   tasks.push(task);
- 
-  //   localStorage.setItem("tasks", JSON.stringify(tasks));
- 
-  //  }
+  let deleteButton = document.createElement("button");
 
-  
+  deleteButton.appendChild(document.createTextNode("Delete"));
+  deleteButton.className =
+    "btn-danger btn btn-sm float-right text-right delete";
 
-   
+  //remove item
+  deleteButton.addEventListener("click", removeListItem);
+
+  li.appendChild(document.createTextNode(task));
+  li.appendChild(deleteButton);
+
+  items.appendChild(li);
+};
+
+// function to remove to do list item
+const removeListItem = (e) => {
+  e.preventDefault();
+  //remove from HTML
+  if (e.target.classList.contains("delete")) {
+    let li = e.target.parentNode;
+    items.removeChild(li);
+  }
+
+  //remove from local storage
+
+  let temp = [];
+  //get HTML items tasks
+  const tasks = Array.from(items.children);
+
+  //push tasks to the new array
+  tasks.forEach((item) => {
+    temp.push(item.firstChild.textContent);
+  });
+
+  //update the local storage without deleted item
+  localStorage.setItem("tasks", JSON.stringify(temp));
+};
+
+const handleLocal = {
+  setLocalItem: (item) => {
+    if (localStorage.getItem("tasks") === null) {
+      let tasks = [];
+      tasks.push(item);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    } else {
+      let tasks = JSON.parse(localStorage.getItem("tasks"));
+      tasks.push(item);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+    console.log("%c Succesfull saved on local storage", "color:green");
+  },
+  getLocalItems: () => {
+    return JSON.parse(localStorage.getItem("tasks"));
+  },
+};
